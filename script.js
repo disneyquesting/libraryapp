3f539daafc1ad417c062477bf151ee1bcd480589
Thomas Lane <tlane@Thomass-MBP.lan>
2020-09-15 20:55:46 -0400

    Close to final

--- script.js
+++ script.js
@@ -35,21 +35,26 @@ function addArray() {
 
     if (!bookTitle) {
         alert("Please enter a Title");
-    } else {
-        myLibrary.push(newBook);
+    } else if (!bookPages) {
+        alert("Please enter the amount of pages in the book.");
+    } else if (!bookAuthor) {
+        alert("Please enter the book's author!")
     }
-    
+    else {
+        myLibrary.push(newBook);
+        formBook.style.display = "none";
+        libraryBook.style.display = "inline";
+    }
+
     booklistArray = [];
     bookCardUpdater(myLibrary);
-
-    formBook.style.display = "none";
 
     return false;
 }
 
 function removeArray(id, bookToRemove) {
     myLibrary.splice(id, bookToRemove);
-   
+
     booklistArray = [];
     bookCardUpdater(myLibrary);
     return false;
@@ -59,29 +64,35 @@ function removeArray(id, bookToRemove) {
 function bookCardUpdater(myLibrary) {
     for (var i = 0; i < myLibrary.length; i++) {
         booklistArray += `<div class="bookCard" id=${myLibrary[i].title}><img class="icon" src="./icons8-book-64.png"/><br><h1>${myLibrary[i].title}</h1><p class="bookAuthor">${myLibrary[i].author}</p><p class="bookPages">Pages: ${myLibrary[i].pages}</p>
-        <br><button id="deleteBook" class="buttonDelete" onclick="removeArray(${myLibrary.indexOf(myLibrary[i])},1)" type="button">Delete</button><button id="readBook${myLibrary.indexOf(myLibrary[i])}" class="readBook" onclick="readBook(${myLibrary.indexOf(myLibrary[i])})" type="button">Mark as Read</button></div>`
+        <br><button id="deleteBook" class="buttonDelete bookButtons" onclick="removeArray(${myLibrary.indexOf(myLibrary[i])},1)" type="button">Delete</button><button id="readBook${myLibrary.indexOf(myLibrary[i])}" class="readBook bookButtons" onclick="readBook(${myLibrary.indexOf(myLibrary[i])})" type="button">Mark as Read</button></div>`
     }
     bookCard.innerHTML = booklistArray;
 }
 
-function readBook (book) {
+function readBook(book) {
     console.log(book);
     myLibrary[book].Read();
     let buttonText = document.querySelector(`#readBook${book}`);
 
-    if (myLibrary[book].read === 'yes'){
+    if (myLibrary[book].read === 'yes') {
         buttonText.innerText = "Mark as Unread";
     } else {
         buttonText.innerText = "Mark as Read";
     }
 }
 
-function formAdd () {
-        formBook.style.display = "inline";
+function formAdd() {
+    document.querySelector('#bookAuthor').value = "";
+    document.querySelector('#bookTitle').value = "";
+    document.querySelector("#bookPages").value = "";
+
+    formBook.style.display = "";
+    libraryBook.style.display = "none";
 }
 
 addBook.addEventListener('click', addArray);
 libraryBook.addEventListener('click', formAdd);
+
 bookCardUpdater(myLibrary);
 
 console.log(myLibrary);
